cd75c95b530b00069af58409f45276399551866f
tutoringisus <mkedkad@gmail.com>
2023-01-18 23:37:19 -0800

    seqcleaner solved

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-sentence = "The old man ran away"
-len(sentence)-1
+class DNAstring(str):
+    def length(self):
+        return (length(self))
+
+    ''' Return an upcased version of the string, collapsing a single run of Ns.'''
+    def purify(self):
+        '''Setting preset type for future use'''
+        pureDNA = ''
+        N_count = 0
+        '''This takes dna and counts the number of N within statement'''
+        for charecters in self:
+            if charecters.upper() == 'N':
+                '''This adds every N counted on each other'''
+                N_count += 1
+            else: #After all ns accounted for, move on the next if statement to compile the int into the string
+                if N_count > 0:
+                    pureDNA += '{' + str(N_count) + '}' #n integer is put within brackets
+                    N_count = 0 #set n back to 0 or it will continue to run if statement
+                pureDNA += charecters.upper() #int is compiled with full purified DNA statement
+        if N_count > 0:
+            pureDNA += '{' + str(N_count) + '}'
+        return pureDNA
+
+
+def main():
+    ''' Get user DNA data and clean it up.'''
+    while (True):
+        data = input('DNA data?')
+        thisDNA = DNAstring(data)
+        pureData = thisDNA.purify()
+        print(pureData)
+
+
+main()
--- testing.py
+++ testing.py
@@ -1,37 +1,47 @@
-#!/usr/bin/env python3 
-# Name: Your full name  
+#!/usr/bin/env python3
+# Name: Your full name
 # Group Members: List full names or “None”
 
 '''
 Read a DNA string from user input and return a collapsed substring of embedded Ns to: {count}.
 
-Example: 
+Example:
  input: AaNNNNNNGTC
 output: AA{6}GTC
 
 Any lower case letters are converted to uppercase
 '''
-#Function to count length of DNA
-class DNAstring:
-  def __init__ (self, dna):
-    self.dna = dna
+class DNAstring(str):
+    def length(self):
+        return (length(self))
 
-  def length(self):
-    return (len(self))
+    ''' Return an upcased version of the string, collapsing a single run of Ns.'''
+    def purify(self):
+        '''Setting preset type for future use'''
+        pureDNA = ''
+        N_count = 0
+        '''This takes dna and counts the number of N within statement'''
+        for charecters in self:
+            if charecters.upper() == 'N':
+                '''This adds every N counted on each other'''
+                N_count += 1
+            else: #After all ns accounted for, move on the next if statement to compile the int into the string
+                if N_count > 0:
+                    pureDNA += '{' + str(N_count) + '}' #n integer is put within brackets
+                    N_count = 0 #set n back to 0 or it will continue to run if statement
+                pureDNA += charecters.upper() #int is compiled with full purified DNA statement
+        if N_count > 0:
+            pureDNA += '{' + str(N_count) + '}'
+        return pureDNA
 
-    # Function to clean DNA 
-  def purify(self):
-    upperdna = self.dna.upper()
-    numbers = upperdna.count("N")
-    purifydna = upperdna.replace("N", "")
-    return purifydna
-   
-#Function that is called that runs data through purifier  
+
 def main():
-  while (True):
-    data = input('DNA data?')
-    thisDNA = DNAstring(data)
-    pureData = thisDNA.purify()
-    print(pureData)
-    
+    ''' Get user DNA data and clean it up.'''
+    while (True):
+        data = input('DNA data?')
+        thisDNA = DNAstring(data)
+        pureData = thisDNA.purify()
+        print(pureData)
+
+
 main()
