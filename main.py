241fa66b63edf0ccac48c7636050b9d7b8bc220a
tutoringisus <mkedkad@gmail.com>
2023-01-18 22:15:28 -0800

    some small tests

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
-print("hello world")
-# This prints my name
-print("My name is Ziad Kedkad.")
-# This prints my username
-print("My username is zkedkad.")
-# This prints my occupation
-print("I am an undergraduate student.")
-# This prints my major
-print("I'm majoring in Molecular Cell Development Biology.")
-# This prints why I am taking this class and I hope to get out of it
-print("I'm taking this class because I believe learning how to code will be a very useful tool in the world of pharma, bioinformatics, genomics, and protein discovery.")
-# This prints the problem in molecular biology that most interests me
-print("The problems I find most interesting is the nervous system and its proliferation and realted stem cell regeneration methods. I'm also interested in protiens responsible for physical proteins such as atp synthase and related transportation proteins.")
-# This prints my prior computer programming experience
-print("This class is the first coding class I have taken. I have watched multiple videos online to prepare and get a foundational understanding of python.")
+sentence = "The old man ran away"
+len(sentence)-1
