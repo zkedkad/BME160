18028ac5aa36442dec0c150783b252294eed02d7
zkedkad@ucsc.edu <zkedkad@ucsc.edu>
2023-01-21 09:47:17 -0800

    need to fix angle output

--- testing.py
+++ testing.py
@@ -1,21 +1,11 @@
-#aminoAcids = ['A', 'D', 'K', 'R']
-#aminoAcids[1:1] = 'W'
-#print(aminoAcids)
-#del aminoAcids[3]
-#print(aminoAcids)
-counts = {'A':0, 'C':0, 'G':0, 'T':0}
-dna = 'ACGTACGT'
+data = 'C = (39.447, 94.657, 11.824) N = (39.292, 95.716, 11.027) Ca = (39.462, 97.101, 11.465)'
+t1 = data.replace ('(', ',') # change ( to ,
+t2 = t1.replace (')', ',')    # change ) to ,
+l  = t2.split (',')           # split on ,
 
-for nuc in dna:
-    counts[nuc] += 1
-    counts[nuc] = counts[nuc] + 1
-
-
-
-emptyDict = dict()       # same as {}
-ThreeToOne = {'Ala':'A', 'Cys':'C'}
-myAA = ThreeToOne['Ala'] # lookup of 1 letter code
-ThreeToOne['Tyr'] = 'Y'  # adds a new AA to dict
-print (ThreeToOne.get('Trp'))
-
-answer = ThreeToOne.get(data) or nucleotide.get(data) or 'I dont know'
+p = (float(l[1]),float(l[2]), float(l[3]))
+q = (float(l[5]),float(l[6]), float(l[7]))
+r = (float(l[9]),float(l[10]), float(l[11]))
+print(p)
+print(q)
+print(r)
