a9c7f8b4faad62e0419f48eedbe0eb5f29d1b5db
zkedkad@ucsc.edu <zkedkad@ucsc.edu>
2023-01-22 11:15:34 -0800

    testing

--- testing.py
+++ testing.py
@@ -1,11 +1,42 @@
-data = 'C = (39.447, 94.657, 11.824) N = (39.292, 95.716, 11.027) Ca = (39.462, 97.101, 11.465)'
-t1 = data.replace ('(', ',') # change ( to ,
-t2 = t1.replace (')', ',')    # change ) to ,
-l  = t2.split (',')           # split on ,
+#!/usr/bin/env python3 
+# Name: Your full name  
+# Group Members: List full names or “None”
 
-p = (float(l[1]),float(l[2]), float(l[3]))
-q = (float(l[5]),float(l[6]), float(l[7]))
-r = (float(l[9]),float(l[10]), float(l[11]))
-print(p)
-print(q)
-print(r)
+'''
+Read a DNA string from user input and return a collapsed substring of embedded Ns to: {count}.
+
+Example: 
+ input: AaNNNNNNGTC
+output: AA{6}GTC
+
+Any lower case letters are converted to uppercase
+'''
+
+
+# Function to count length of DNA
+class DNAstring:
+    def __init__(self, dna):
+        self.dna = dna
+
+    def length(self):
+        return (len(self))
+
+        # Function to clean DNA
+
+    def purify(self):
+        upperdna = self.dna.upper()
+        comma_data = upperdna.replace("", ",")
+        list_data = comma_data.split(",")
+        print(list_data)
+        return
+
+# Function that is called that runs data through purifier
+def main():
+    while (True):
+        data = input('DNA data?')
+        thisDNA = DNAstring(data)
+        pureData = thisDNA.purify()
+        print(pureData)
+
+
+main()
