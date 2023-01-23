105dfb9d483c48b173143677297aa4b078543698
zkedkad@ucsc.edu <zkedkad@ucsc.edu>
2023-01-22 16:31:22 -0800

    updated

--- testing.py
+++ testing.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python3 
-# Name: Your full name  
+# Name: Your full name 
 # Group Members: List full names or “None”
 
 '''
-Read a DNA string from user input and return a collapsed substring of embedded Ns to: {count}.
-
-Example: 
- input: AaNNNNNNGTC
-output: AA{6}GTC
-
-Any lower case letters are converted to uppercase
+A dictionary is stored for short and long amino acids as well as rna and dna codons. 
+This is used to help convert amino acid and nucleotide terms into its shorter or
+longer definition for decoding genetic information. 
 '''
+short_AA = {
+            'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',#three letter to one letter amino acid
+            'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',#amino acid conversion is stored
+            'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
+            'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'
+            }
 
+long_AA = {value:key for key,value in short_AA.items()} #this converts value so that the dictionary can be used reverse
+#below is stored information to convert amino acid to codon 
+RNA_codon_table = {
+'UUU': 'Phe', 'UCU': 'Ser', 'UAU': 'Tyr', 'UGU': 'Cys',#U
+'UUC': 'Phe', 'UCC': 'Ser', 'UAC': 'Tyr', 'UGC': 'Cys',
+'UUA': 'Leu', 'UCA': 'Ser', 'UAA': '---', 'UGA': '---',
+'UUG': 'Leu', 'UCG': 'Ser', 'UAG': '---', 'UGG': 'Trp',
+'CUU': 'Leu', 'CCU': 'Pro', 'CAU': 'His', 'CGU': 'Arg',#C
+'CUC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg',
+'CUA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg',
+'CUG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg',
+'AUU': 'Ile', 'ACU': 'Thr', 'AAU': 'Asn', 'AGU': 'Ser',#A
+'AUC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser',
+'AUA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg',
+'AUG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg',
+'GUU': 'Val', 'GCU': 'Ala', 'GAU': 'Asp', 'GGU': 'Gly',#G
+'GUC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly',
+'GUA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly',
+'GUG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly'
+}
+dnaCodonTable = {value.replace('U','T'):key for key, value in RNA_codon_table.items()}
+#above tells the U to convert to T in the case that a DNA value is inputted to find the reverse data value
 
-# Function to count length of DNA
-class DNAstring:
-    def __init__(self, dna):
-        self.dna = dna
-
-    def length(self):
-        return (len(self))
-
-        # Function to clean DNA
-
-    def purify(self):
-        upperdna = self.dna.upper()
-        count_n = upperdna.count("N")
-        find_n = upperdna.find("N")
-        list_dna = list(upperdna)
-        list_dna[find_n] = count_n
-        string_dna = str(list_dna)
-        final_dna = "".join(string_dna)
-        print(final_dna)
-        return 
-
-# Function that is called that runs data through purifier
+#below is the function that takes inputted user data and inputs the value within each dictionary to find
+#the definition. It then prints the result or prints unknown in the case that nothing is found.
 def main():
     while (True):
-        data = input('DNA data?')
-        thisDNA = DNAstring(data)
-        pureData = thisDNA.purify()
-        print(pureData)
-
+        data = input("Input what you want to convert?  ")
+        answer = short_AA.get(data) or long_AA.get(data) or RNA_codon_table.get(data) or dnaCodonTable.get(data) or 'Unknown'
+        print(answer)
+    pass
 
 main()
