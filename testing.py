b5c2264f7c0f0ddf536ad26badc94215c8eba587
zkedkad@ucsc.edu <zkedkad@ucsc.edu>
2023-01-31 23:39:40 -0800

    testing

--- testing.py
+++ testing.py
@@ -1,49 +1,6 @@
-#!/usr/bin/env python3 
-# Name: Your full name 
-# Group Members: List full names or “None”
-
-'''
-A dictionary is stored for short and long amino acids as well as rna and dna codons. 
-This is used to help convert amino acid and nucleotide terms into its shorter or
-longer definition for decoding genetic information. 
-'''
-short_AA = {
-            'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',#three letter to one letter amino acid
-            'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',#amino acid conversion is stored
-            'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
-            'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'
-            }
-
-long_AA = {value:key for key,value in short_AA.items()} #this converts value so that the dictionary can be used reverse
-#below is stored information to convert amino acid to codon 
-RNA_codon_table = {
-'UUU': 'Phe', 'UCU': 'Ser', 'UAU': 'Tyr', 'UGU': 'Cys',#U
-'UUC': 'Phe', 'UCC': 'Ser', 'UAC': 'Tyr', 'UGC': 'Cys',
-'UUA': 'Leu', 'UCA': 'Ser', 'UAA': '---', 'UGA': '---',
-'UUG': 'Leu', 'UCG': 'Ser', 'UAG': '---', 'UGG': 'Trp',
-'CUU': 'Leu', 'CCU': 'Pro', 'CAU': 'His', 'CGU': 'Arg',#C
-'CUC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg',
-'CUA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg',
-'CUG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg',
-'AUU': 'Ile', 'ACU': 'Thr', 'AAU': 'Asn', 'AGU': 'Ser',#A
-'AUC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser',
-'AUA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg',
-'AUG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg',
-'GUU': 'Val', 'GCU': 'Ala', 'GAU': 'Asp', 'GGU': 'Gly',#G
-'GUC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly',
-'GUA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly',
-'GUG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly'
-}
-dnaCodonTable = {value.replace('U','T'):key for key, value in RNA_codon_table.items()}
-#above tells the U to convert to T in the case that a DNA value is inputted to find the reverse data value
-
-#below is the function that takes inputted user data and inputs the value within each dictionary to find
-#the definition. It then prints the result or prints unknown in the case that nothing is found.
-def main():
-    while (True):
-        data = input("Input what you want to convert?  ")
-        answer = short_AA.get(data) or long_AA.get(data) or RNA_codon_table.get(data) or dnaCodonTable.get(data) or 'Unknown'
-        print(answer)
-    pass
-
-main()
+string = "horse"
+vowels = {"a", "e", "i", "o", "u"}
+for i in string:
+    if string in vowels:
+        count += 1
+        print(count)
