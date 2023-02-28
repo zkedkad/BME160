0dbe1c6b773c8156254800985a79068c36822531
zkedkad@ucsc.edu <zkedkad@ucsc.edu>
2023-02-27 22:11:26 -0800

    added file output code

--- Lab05/findORFs.py
+++ Lab05/findORFs.py
@@ -52,34 +52,81 @@ class CommandLine() :
 # 
 #
 ########################################################################
-   
-def main(MP=None):
+from sequenceAnalysis import NucParams, ProteinParam, FastAreader, Orf_Finder
+import sys
+def output(infile, outfile):
+
+    fastaReader = FastAreader(infile)
+    orfsFound = []
+
+    f = open(outfile, 'w')
+    sys.stdout = f
+
+    for header, sequence in fastaReader.readFasta():
+        print(header)
+
+        '''find forward and reverse comp ORFs in sequence'''
+        finder = Orf_Finder(sequence)
+        orfList = finder.locate_ORFs()
+        revOrfList = finder.locate_reverse_ORFs()
+
+        '''convert orfList to clear tuples in single list for sorting'''
+        for list in orfList:
+            for tuple in list:
+                frame = tuple[0] + 1
+                startPos = tuple[1] + 1
+                endPos = tuple[2]
+                orfsFound.append((frame, startPos, endPos, tuple[3]))
+
+        '''covert reverse complementary ORFs to top strand coordinates and add to single list'''
+        for list in revOrfList:
+            for tuple in list:
+                frame = tuple[0] + 1
+                startPos = tuple[1] + 1
+                endPos = tuple[2]
+                orfsFound.append((-frame, len(sequence)-endPos+1,
+                                  len(sequence)-startPos+1, tuple[3]))
+
+    '''sort by largest to smallest and by start position furthest to the left'''
+    orfsFound.sort(key=lambda tup: (tup[3], tup[1]), reverse=True)
+
+    '''print out list to file in proper format'''
+    for orf in orfsFound:
+        print('{:+d} {:>5d}..{:>5d} {:>5d}'.format(orf[0], orf[1], orf[2], orf[3]))
+
+    f.close()
+
+########################################################################
+#
+# Main
+#
+########################################################################
+
+def main(myCommandLine=None):
     '''
-    Reads in a fasta file and outputs the ORFs frame, start, stop, 
-    and length position on a output file.
+    Implements the Usage exception handler that can be raised from anywhere in process.
     '''
-    if MP is None:
-        main_program = CommandLine()
-        if main_program.args.longestGene:
-            areader_file = FastAreader()
-            for header, sequence in areader_file.readFasta():
-                print(header)
-                orfData = Orf_Finder(sequence)
-                orfData.locate_ORFs()
-                orfData.locate_reverse_ORFs()
-                fil_list = filter(lambda orf: orf[3] > main_program.args.minGene, orfData.orfs)
-                for reading_frame, start, stop, size, in sorted(fil_list, key=lambda orf:orf[3], reverse = True):
-                    print('{:+d} {:>5d}..{:>5d} {:>5d}'.format(reading_frame, start, stop, size))
-            else:
-                main_program = CommandLine(MP)
-            print(main_program.args)
-    ###### replace the code between comments.
-    # thisCommandLine.args.longestGene is True if only the longest Gene is desired
-    # thisCommandLine.args.start is a list of start codons
-    # thisCommandLine.args.stop is a list of stop codons
-    # thisCommandLine.args.minGene is the minimum Gene length to include
-    #
-    #######
-    
+    if myCommandLine is None:
+        myCommandLine = CommandLine(['tass2.fa',
+                                     'tass2ORFdata-ATG-100.txt',
+                                     '--longestGene',
+                                     '--start=ATG',
+                                     '--minGene=100'])
+
+        output(myCommandLine.args.inFile, myCommandLine.args.outFile)
+    else:
+        myCommandLine = CommandLine(myCommandLine)
+        #output(myCommandLine.args.inFile, myCommandLine.args.outFile)
+        ###### replace the code between comments.
+        # myCommandLine.args.inFile has the input file name
+        # myCommandLine.args.outFile has the output file name
+        # myCommandLine.args.longestGene is True if only the longest Gene is desired
+        # myCommandLine.args.start is a list of start codons
+        # myCommandLine.args.minGene is the minimum Gene length to include
+        #
+        myReader = FastAreader(myCommandLine.args.inFile)
+
+#######
+
 if __name__ == "__main__":
     main()
