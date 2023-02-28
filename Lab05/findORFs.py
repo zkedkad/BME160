675ec40a94650f9c1640d91cd741cc07a1ea774b
zkedkad@ucsc.edu <zkedkad@ucsc.edu>
2023-02-27 22:14:18 -0800

    edited arg code

--- Lab05/findORFs.py
+++ Lab05/findORFs.py
@@ -6,45 +6,49 @@ from sequenceAnalysis import Orf_Finder, FastAreader, ProteinParam, NucParams
 ########################################################################
 # CommandLine
 ########################################################################
-class CommandLine() :
+class CommandLine():
     '''
     Handle the command line, usage and help requests.
-
-    CommandLine uses argparse, now standard in 2.7 and beyond. 
+    CommandLine uses argparse, now standard in 2.7 and beyond.
     it implements a standard command line argument parser with various argument options,
-    a standard usage and help.
-
+    a standard usage and help, and an error termination mechanism do-usage_and_die.
     attributes:
     all arguments received from the commandline using .add_argument will be
     avalable within the .args attribute of object instantiated from CommandLine.
     For example, if myCommandLine is an object of the class, and requiredbool was
     set as an option using add_argument, then myCommandLine.args.requiredbool will
     name that option.
- 
     '''
-    
-    def __init__(self, inOpts=None) :
+
+    def __init__(self, inOpts=None):
         '''
-        Implement a parser to interpret the command line argv string using argparse.
+        CommandLine constructor.
+        Implements a parser to interpret the command line argv string using argparse.
         '''
-        
+
         import argparse
-        self.parser = argparse.ArgumentParser(description = 'Program prolog - a brief description of what this thing does', 
-                                             epilog = 'Program epilog - some other stuff you feel compelled to say', 
-                                             add_help = True, #default is True 
-                                             prefix_chars = '-', 
-                                             usage = '%(prog)s [options] -option1[default] <input >output'
-                                             )
-        self.parser.add_argument('-lG', '--longestGene', action = 'store', nargs='?', const=True, default=False, help='longest Gene in an ORF')
-        self.parser.add_argument('-mG', '--minGene', type=int, choices= (100,200,300,500,1000), default=100, action = 'store', help='minimum Gene length')
-        self.parser.add_argument('-s', '--start', action = 'append', default = ['ATG'],nargs='?', 
-                                 help='start Codon') #allows multiple list options
-        self.parser.add_argument('-t', '--stop', action = 'append', default = ['TAG','TGA','TAA'],nargs='?', help='stop Codon') #allows multiple list options
-        self.parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')  
-        if inOpts is None :
+        self.parser = argparse.ArgumentParser(
+            description='Program prolog - a brief description of what this thing does',
+            epilog='Program epilog - some other stuff you feel compelled to say',
+            add_help=True,  # default is True
+            prefix_chars='-',
+            usage='%(prog)s [options] -option1[default] <input >output'
+            )
+        self.parser.add_argument('inFile', action='store', help='input file name')
+        self.parser.add_argument('outFile', action='store', help='output file name')
+        self.parser.add_argument('-lG', '--longestGene', action='store', nargs='?', const=True, default=True,
+                                 help='longest Gene in an ORF')
+        self.parser.add_argument('-mG', '--minGene', type=int, choices=range(0, 2000), action='store',
+                                 help='minimum Gene length')
+        self.parser.add_argument('-s', '--start', action='append', nargs='?',
+                                 help='start Codon')  # allows multiple list options
+        self.parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
+
+        if inOpts is None:
             self.args = self.parser.parse_args()
-        else :
+        else:
             self.args = self.parser.parse_args(inOpts)
+
 
 ########################################################################
 # Main
